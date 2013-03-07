50a6b35cec0ecb6fbfc95b9b972384a3a3807e29
Adam Groszer <agroszer@gmail.com>
2013-03-07 13:02:35 +0000

    adding missing pieces

---
+++ Python24_32_clean/setupcompilerandexecute.bat
@@ -0,0 +1,6 @@
+set PATH=%PATH%;c:\mingw\bin
+rem make zc.buildout happy:
+set PYTHON2.5=c:\Python25_32_clean\python.exe
+set PYTHON2.6=c:\Python26_32_clean\python.exe
+set PYTHON2.7=c:\Python27_32_clean\python.exe
+%*
