3c1df1c2bc86aed75a983e5a1b356e515439b96b
Adam Groszer <agroszer@gmail.com>
2013-03-07 13:57:44 +0100

    adding missing pieces

---
+++ cleanfolder.py
@@ -0,0 +1,23 @@
+# Delete everything reachable from the directory named in 'top',
+# assuming there are no symbolic links.
+# CAUTION:  This is dangerous!  For example, if top == '/', it
+# could delete all your disk files.
+import os
+import sys
+import stat
+
+is_win32 = sys.platform == 'win32'
+
+top = sys.argv[1]
+
+for root, dirs, files in os.walk(top, topdown=False):
+    for name in files:
+        fname = os.path.join(root, name)
+        if is_win32:
+            os.chmod(fname, stat.S_IWRITE)
+        os.remove(fname)
+        print "erase %s" % fname
+    for name in dirs:
+        dname = os.path.join(root, name)
+        os.rmdir(dname)
+        print "rmdir %s" % dname
