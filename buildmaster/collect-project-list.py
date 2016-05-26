0600b793db93355e02da9645a57bae6b136f801c
Adam Groszer <agroszer@gmail.com>
2016-05-26 15:25:11 +0200

    collect supported python versions

---
+++ buildmaster/collect-project-list.py
@@ -0,0 +1,52 @@
+# collect supported python versions for project-list.cfg
+
+# first parameter must be a PATH to a local checkout of all projects
+# handy helper: https://github.com/mgedmin/cloneall
+
+import os
+import re
+import sys
+
+localCheckouts = sys.argv[1]
+
+platforms = ('2.7', '3.3', '3.4', '3.5')
+
+projects = open("project-list.cfg", "rb").readlines()
+projects = [x.strip() for x in projects]
+
+output = []
+
+for project in projects:
+    if not project or project.startswith('#'):
+        output.append(project)
+        continue  # comment or empty line
+
+    parts = project.split(',')
+    if len(parts) == 3:
+        projectName, repourl, versions = parts
+    elif len(parts) == 2:
+        projectName, repourl = parts
+        version = ''
+    else:
+        print "Failed on line %s, ignoring", project
+        continue
+    project = project.strip()
+    repourl = repourl.strip()
+
+    localPath = os.path.join(localCheckouts, projectName)
+    if os.path.exists(localPath):
+        setuppy = os.path.join(localPath, 'setup.py')
+        if os.path.exists(setuppy):
+            content = open(setuppy, "rb").read()
+            versions = []
+            # this is damn ugly, but does the job
+            for m in re.finditer(r"Programming Language :: Python :: (\d.\d)",
+                                 content):
+                ver = m.group(1)
+                if ver in platforms:
+                    versions.append(ver)
+            version = ';'.join(versions)
+    line = "%s,%s,%s" % (projectName, repourl, version)
+    output.append(line)
+
+open("project-list.cfg", "wb").write("\n".join(output))
